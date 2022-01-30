7dfa119547531392d134d9dc378ab41ac0fdba9f
Charlene Kwok <charlenekwok499@gmail.com>
2022-01-30 19:16:03 +1100

    fetch data from db

--- food-order/src/components/Main/AvailableMeal.js
+++ food-order/src/components/Main/AvailableMeal.js
@@ -1,38 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeal.module.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeal = () => {
+  const [meal, setMeal] = useState([]);
+  useEffect(() => {
+    const getData = async (second) => {
+      const resp = await fetch(
+        "https://react-meal-b0713-default-rtdb.firebaseio.com/meals.json",
+        {
+          method: "GET",
+        }
+      );
+      const data = await resp.json();
+      setMeal(Object.values(data));
+    };
+    getData();
+  }, []);
+
   return (
     <div className={classes.container}>
-      {DUMMY_MEALS.map((meal) => (
+      {meal?.map((meal) => (
         <MealItem meal={meal} key={`${meal.id}-item`} />
       ))}
     </div>
